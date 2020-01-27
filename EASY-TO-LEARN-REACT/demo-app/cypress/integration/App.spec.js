656edb36699010e1e8e442e56f4fd4db41b6445b
rlazcano <rlazcano@redlab.com.mx>
2020-01-27 11:06:56 -0600

    se agrega configuracion fuincional para utilizar cypress

--- EASY-TO-LEARN-REACT/demo-app/cypress/integration/App.spec.js
+++ EASY-TO-LEARN-REACT/demo-app/cypress/integration/App.spec.js
@@ -1,6 +1,6 @@
 describe('App E2E', () => {
     it('should assert that true is equal to true', () => {
-        cy.visit("/");
+        cy.visit('/');
       //expect(true).to.equal(true);
     });
   });
